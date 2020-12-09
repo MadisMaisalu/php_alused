6f5bead22cfe4426b6672f63b16cb95e09eac4b4
Madis <madismaisalumm@gmail.com>
2020-12-09 10:02:41 +0200

    ül5 peaaegu valmis

--- Ülesanne 5/yl5.php
+++ Ülesanne 5/yl5.php
@@ -9,20 +9,20 @@ foreach($tydrukud as $tydruk) {
 echo $tydrukud[0], " ", $tydrukud[1], " ", $tydrukud[2], "<br>";
 echo $tydrukud[7],"<br>";
 echo $tydrukud[array_rand($tydrukud)];
-"<br>";
-"<br>";
-"<br>";
-"<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
 $autod = array("Subaru","BMW","Acura","Mercedes-Benz","Lexus","GMC","Volvo","Toyota","Volkswagen","Volkswagen","GMC","Jeep","Saab","Hyundai","Subaru","Mercedes-Benz",
     "Honda","Kia","Mercedes-Benz","Chevrolet","Chevrolet","Porsche","Buick","Dodge","GMC","Dodge","Nissan","Dodge","Jaguar","Ford","Honda","Toyota","Jeep",
     "Kia","Buick","Chevrolet","Subaru","Chevrolet","Chevrolet","Pontiac","Maybach","Chevrolet","Plymouth","Dodge","Nissan","Porsche","Nissan","Mercedes-Benz",
     "Suzuki","Nissan","Ford","Acura","Volkswagen","Lincoln","Mazda","BMW","Mercury","Mitsubishi","Ram","Audi","Kia","Pontiac","Toyota","Acura","Toyota","Toyota",
     "Chevrolet","Oldsmobile","Acura","Pontiac","Lexus","Chevrolet","Cadillac","GMC","Jeep","Audi","Acura","Acura","Honda","Dodge","Hummer","Chevrolet","BMW",
     "Honda","Lincoln","Hummer","Acura","Buick","BMW","Chevrolet","Cadillac","BMW","Pontiac","Audi","Hummer","Suzuki","Mitsubishi","Jeep","Buick","Ford");
-"<br>";
-"<br>";
-"<br>";
-"<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
 $vin = array("1GKS1GKC8FR966658", "1FTEW1C87AK375821", "1G4GF5E30DF760067", "1FTEW1CW9AF114701", "WAUGGAFC8CN433989", "3G5DA03E83S704506", "4JGDA2EB0DA207570",
     "1FTEW1E88AK070552", "SAJWA0F77F8732763", "JHMFA3F21BS660717", "JTHBP5C29C5750730", "WA1LFAFP9DA963060", "3D7TT2CT6BG521976", "WVWN7EE961049",
     "2C3CA5CG3BH341234", "YV4952CFXC162587", "KNALN4D71F5805172", "JN1CV6EK7BM903692", "5FRYD3H84EB186765", "WAUL64B83N441878", "WDDGF4HBXCF845665",
@@ -38,7 +38,7 @@ $vin = array("1GKS1GKC8FR966658", "1FTEW1C87AK375821", "1G4GF5E30DF760067", "1FT
     "5GAER13D19J026924", "1G4HC5EM1BU329204", "3VWML7AJ6CM772736", "3C6TD4HT2CG011211", "JTDZN3EU2FJ023675", "JN8AZ1MU4CW041721", "KNAFX5A82F5991024",
     "1N6AA0CJ1D57470", "WAUEG98E76A780908", "WAUAF78E96A920706", "1GT01XEG8FZ268942", "1FTEW1CW4AF371278", "JN1AZ4EH8DM531691", "WAUEKAFBXAN294295",
     "1N6AA0EDXFN868772", "WBADW3C59DJ422810");
-"<br>";
+echo "<br>";
 echo count($autod);
 
 if (count($autod) != count($vin)) {
@@ -47,16 +47,82 @@ if (count($autod) != count($vin)) {
 else {
     echo "Listid on ühepikkused";
 }
-"<br>";
-"<br>";
+echo "<br>";
+echo "<br>";
 $counted = array_count_values($autod);
 $numberOfAudi = $counted['Audi'];
 $numberOfToyota = $counted['Toyota'];
-echo "Audide arv on ".$numberOfAudi;
+echo "Audide arv on ".$numberOfAudi."<br>";
 echo "Toyotade arv on ".$numberOfToyota;
-"<br>";
-"<br>";
-"<br>";
-"<br>";
-"<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+foreach ($vin as $vinkood) {
+    if(strlen($vinkood) < 17){
+        echo $vinkood.'<br>';
+    }
+}
+echo "<br>";
+echo "<br>";
+echo "<br>";
 
+$palgad = array(1220,1213,1295,1312,1298,1354,1296,1286,1292,1327,1369,1455);
+$keskmine = array_sum($palgad) / count($palgad);
+echo $keskmine."<br>";
+echo "<br>";
+echo "<br>";
+
+$firmad=array("Kimia","Mynte","Voomm","Twiyo","Layo","Talane","Gigashots","Tagchat","Quaxo","Voonyx","Kwilith","Edgepulse","Eidel","Eadel","Jaloo","Oyope","Jamia");
+foreach ($firmad as $firma) {
+    echo $firma."<br>";
+}
+echo "<br>";
+echo "<br>";
+$riigid = array("Indonesia","Canada","Kyrgyzstan","Germany","Philippines",
+    "Philippines","Canada","Philippines","South Sudan","Brazil",
+    "Democratic Republic of the Congo","Indonesia","Syria","Sweden",
+    "Philippines","Russia","China","Japan","Brazil","Sweden","Mexico","France",
+    "Kazakhstan","Cuba","Portugal","Czech Republic");
+echo "<br>";
+echo "<br>";
+echo "<br>";
+$maxlen = max(array_map('strlen', $riigid));
+
+
+foreach ($riigid as $riik) {
+    if(strlen($riik) == $maxlen){
+        echo $riik.'<br>';
+    }
+}
+echo "<br>";
+echo "<br>";
+echo "<br>";
+$hiinanimed = array("瀚聪","月松","雨萌","展博","雪丽","哲恒","慧妍","博裕","宸瑜","奕漳",
+    "思宏","伟菘","彦歆","睿杰","尹智","琪煜","惠茜","晓晴","志宸","博豪",
+    "璟雯","崇杉","俊誉","军卿","辰华","娅楠","志宸","欣妍","明美");
+
+sort($hiinanimed);
+echo $hiinanimed[0]."<br>".end($hiinanimed);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$google = array("Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","
+Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin",
+"Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins",
+"Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey",
+"Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes");
+
+
+$string = 'Bradwell';
+foreach ($google as $moogle) {
+    //if (strstr($string, $url)) { // mine version
+    if (strpos($string, $moogle) !== FALSE) { // Yoshi version
+        echo "Match found";
+        return true;
+    }
+}
+echo "Not found!";
+return false;
