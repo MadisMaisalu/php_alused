be3db31e456fe59d944b46146af3fcfc74961e05
Madis <madismaisalumm@gmail.com>
2021-01-05 11:50:00 +0200

    ül5 peaaegu valmis

---
+++ Ülesanne 6/yl6.php
@@ -0,0 +1,28 @@
+<form>
+    <input name="laius">
+    <label for="laius">Laius</label><br>
+    <br>
+    <input name="korgus">
+    <label for="korgus">Kõrgus</label>
+</form>
+
+
+<?php
+$number = 1;
+do{
+    echo $number.".".'<br>';
+    $number++;
+} while($number <=100);
+echo '<br>';
+echo "*********";
+echo '<br>';
+echo '<br>';
+$kordus = 1;
+do{
+    echo '*'.'<br>';
+    $kordus++;
+} while($kordus <=10);
+
+do {
+    echo
+}
